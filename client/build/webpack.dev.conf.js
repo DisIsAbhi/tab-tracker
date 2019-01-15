37f378772734c8c9d23ba118bfd4b7e3591e0b31
sai vs <svutukuru@hotmail.com>
2019-01-14 21:15:12 -0500

    setup express

--- client/build/webpack.dev.conf.js
+++ client/build/webpack.dev.conf.js
@@ -78,11 +78,11 @@ module.exports = new Promise((resolve, reject) => {
       process.env.PORT = port
       // add port to devServer config
       devWebpackConfig.devServer.port = port
-
+      
       // Add FriendlyErrorsPlugin
       devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
         compilationSuccessInfo: {
-          messages: [` Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`]
+          messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`]
         },
         onErrors: config.dev.notifyOnErrors
           ? utils.createNotifierCallback()
