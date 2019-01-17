57a1c181b181e025dbfb2f89fb3d7c93ea743153
sai vs <svutukuru@hotmail.com>
2019-01-17 01:02:22 -0500

    updated code upto 20190117 for email validation

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -10,7 +10,6 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      historyApiFallback: true,
       component: HelloWorld
     },
     {
--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-
+const { sequelize } = require('./models')
+const config = require('./config/config')
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
@@ -15,9 +16,11 @@ app.use(cors())
 //   })
 // })
 
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
+require('./routes')(app)
+
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Server started on port ${config.port}`)
   })
-})
-app.listen(process.env.PORT || 8085)
+app.listen(process.env.PORT || 8082)
